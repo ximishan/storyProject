8061bbeb0678cb879818ea6fc0cafc580afe9205
zhangfeng <908980328@qq.com>
2026-08-30 16:12:13 +0800

    启用父女日常精简创作界面

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import { installBrowserMock } from "./browserMock";
+import { installSingleDadUiAdapter } from "./singleDadUi";
 
 installBrowserMock();
+installSingleDadUiAdapter();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
